d89194dc1fa98c56b17a866c509b69376eed6f7d
Hurmat Ilyas <hurmat2019@namal.edu.pk>
2021-10-02 20:43:46 +0500

    Mul Functionality

---
+++ cal04.py
@@ -0,0 +1,15 @@
+#Calculator in Python(v0.4)
+def sub(num1,num2):
+  return num1-num2
+
+def add(num1,num2):
+  return num1 + num2
+
+def div(num1,num2):
+	return num1/num2
+
+def mul(num1,num2):
+	return num1*num2
+
+#Main
+print(mul(4,3),div(4,2),sub(4,2),add(7,1))
