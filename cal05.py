571e676ae9531b9a15930d9c24c6afd48508b8be
Muhammad Bilal <mbilal2019@namal.edu.pk>
2021-10-02 21:37:29 +0500

    Sqroot Functionality

---
+++ cal05.py
@@ -0,0 +1,18 @@
+#Calculator in Python(v0.5)
+def sub(num1,num2):
+  return num1-num2
+
+def add(num1,num2):
+  return num1 + num2
+
+def div(num1,num2):
+	return num1/num2
+
+def mul(num1,num2):
+	return num1*num2
+
+def sqRoot(num1):
+	return num1**0.5
+
+#Main
+print(sqroot(25),mul(4,3),div(4,2),sub(4,2),add(7,1))
